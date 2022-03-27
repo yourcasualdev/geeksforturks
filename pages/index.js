2d4c437abad12d9e5ad16d371ee41d7d3f7fdd63
babal <babalibrahim34@gmail.com>
2022-03-27 12:47:44 +0300

    Added googleads to main site

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Footer from '../components/Footer'
+import GoogleAds from '../components/GoogleAds'
 import Header from '../components/Header'
 import { CategoryShowCase } from '../components/MainSiteComponents'
 
@@ -12,6 +13,7 @@ export default function Home() {
         <meta name="description" content="Geeks for türks anasayfa" />
         <meta name="Keywords" content="HTML, Python, CSS, SQL, JavaScript, How to, PHP, Java, C, C++, C#, jQuery, Bootstrap, Colors, W3.CSS, XML, MySQL, Icons, Node.js, React, Graphics, Angular, R, AI, Git, Data Science, Code Game, Tutorials, Programming, Web Development, Training, Learning, Quiz, Exercises, Courses, Lessons, References, Examples, Learn to code, Source code, Demos, Tips, Website"></meta>
         <link rel="icon" href="/favicon.ico" />
+        <GoogleAds />
       </Head>
       <Header />
       <main>
